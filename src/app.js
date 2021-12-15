c541b35cec452ec2a08cad2961e81c6cb4e40592
Nikunj Aggarwal <nikunj.aggarwal@tothenew.com>
2021-12-15 19:10:11 +0530

    Setting up code style using eslint

--- src/app.js
+++ src/app.js
@@ -1,19 +1,19 @@
-require("dotenv").config();
+require('dotenv').config();
 
-const express = require("express");
-const helmet = require("helmet");
-const morgan = require("morgan");
+const express = require('express');
+const helmet = require('helmet');
+const morgan = require('morgan');
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(morgan("dev"));
+app.use(morgan('dev'));
 app.use(helmet());
 
-app.get("/", (req, res, next) => {
-	res.send("Hello");
+app.get('/', (req, res, next) => {
+  res.send('Hello');
 });
 
 module.exports = app;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-const app = require("./app");
+const app = require('./app');
 
 const PORT = process.env.PORT || 8000;
 
 app.listen(PORT, () => {
-	console.log(`App listening on http://localhost:${PORT}`);
+  console.log(`App listening on http://localhost:${PORT}`);
 });
