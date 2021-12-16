34ed8c789e64a1fa200bf6bbe75dd2124aca97c0
Nikunj Aggarwal <nikunj.aggarwal@tothenew.com>
2021-12-16 14:11:33 +0530

    Added cors for the client origin

--- src/app.js
+++ src/app.js
@@ -3,16 +3,21 @@ require('dotenv').config();
 const express = require('express');
 const helmet = require('helmet');
 const morgan = require('morgan');
+const cors = require('cors');
+const passport = require('passport');
 
 const api = require('./api/index');
 
 const app = express();
+
+require('./auth/google-sso');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(morgan('dev'));
 app.use(helmet());
+app.use(cors({ origin: 'http://localhost:3000', credentials: true }));
 
 app.use(
   require('express-session')({
@@ -21,6 +26,9 @@ app.use(
     resave: false,
   })
 );
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use('/api/v1', api);
 
