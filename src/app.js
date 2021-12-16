950e8ebc2ee6fbda5d759204ea2fd5fbb4049e53
Nikunj Aggarwal <nikunj.aggarwal@tothenew.com>
2021-12-16 14:07:38 +0530

    Added express-session for maintaining a session on server

--- src/app.js
+++ src/app.js
@@ -14,6 +14,14 @@ app.use(express.urlencoded({ extended: true }));
 app.use(morgan('dev'));
 app.use(helmet());
 
+app.use(
+  require('express-session')({
+    secret: process.env.SECRET,
+    saveUninitialized: false,
+    resave: false,
+  })
+);
+
 app.use('/api/v1', api);
 
 module.exports = app;
